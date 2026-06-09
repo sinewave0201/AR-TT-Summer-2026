e97b445122ece461f4268cab5787949cb233ec8d
sinewave0201 <silver520060201@gmail.com>
2026-06-09 17:34:00 -0400

    update before downgrading into unity 2022
    the whole project is of such a high version that it cannot be implemented into the andriod laptop i borrowed.. i am degrading the whole version so that it fits in

---
+++ Packages/com.unity.xr.arfoundation/Runtime/ARSubsystems/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("Unity.XR.ARSubsystems.Editor")]
+[assembly: InternalsVisibleTo("Unity.XR.ARSubsystems.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.XR.ARFoundation.Runtime.Tests")]
+#endif
